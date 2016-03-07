10e7bff9c81f6f421667b5a5f31fd38a93aafb9c
neskoskin <neskoskin@gmail.com>
2016-03-07 19:11:22 +0100

    Added mongo

--- server/routes/transactionRoutes.js
+++ server/routes/transactionRoutes.js
@@ -4,20 +4,39 @@
   var router = express.Router();
   var mongojs = require('mongojs');
   var db = mongojs('PersonalAppDb', ['transactions']);
+  var APP_CONFIG = {
+   "mongo": {
+      "hostString": "mongodb:27017/9c77e7c51b283e4a9c2e58a79be3ad70",
+      "user": "9c77e7c51b283e4a9c2e58a79be3ad70",
+      "db": "9c77e7c51b283e4a9c2e58a79be3ad70"
+   }
+};
+var config = JSON.parse(APP_CONFIG);
+    var MongoClient = require('mongodb').MongoClient;
 
+    MongoClient.connect(
+    	"mongodb://" + config.mongo.user + ":kartagina@" +
+		config.mongo.hostString,
+    	function(err, db) {
+            if(!err) {
+                res.end("We are connected to MongoDB\n");
+            } else {
+                res.end("Error while connecting to MongoDB\n");
+            }
+    });
   /* GET home page. */
   router.get('/', function(req, res) {
     res.render('index');
   });
 
   router.get('/api/transactions', function(req, res) {
-    db.transactions.find(function(err, data) {
+    MongoClient.transactions.find(function(err, data) {
       res.json(data);
     });
   });
 
   router.post('/api/transactions', function(req, res) {
-    db.transactions.insert(req.body, function(err, data) {
+    MongoClient.transactions.insert(req.body, function(err, data) {
       res.json(data);
     });
 
@@ -25,7 +44,7 @@
 
   router.put('/api/transactions', function(req, res) {
 
-    db.transactions.update({
+    MongoClient.transactions.update({
       _id: mongojs.ObjectId(req.body._id)
     }, {
       isCompleted: req.body.isCompleted,
@@ -37,7 +56,7 @@
   });
 
   router.delete('/api/transactions/:_id', function(req, res) {
-    db.transactions.remove({
+    MongoClient.transactions.remove({
       _id: mongojs.ObjectId(req.params._id)
     }, '', function(err, data) {
       res.json(data);
